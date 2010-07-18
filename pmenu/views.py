5952ce446b18143cc9beee4619711bd3bacc3b00
Marko Krause <zeratul2099@googlemail.com>
2010-07-18 20:22:17 +0200

    nice add/del-buttons, menu-settings

--- pmenu/views.py
+++ pmenu/views.py
@@ -19,7 +19,7 @@ from puente.pmenu.models import Category, MenuItem
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import Context, loader
-
+from django.db import IntegrityError
 
 def showMenu(request):
     itemDict = {}
@@ -30,6 +30,37 @@ def showMenu(request):
     return render_to_response("pmenu_list.html", { "itemDict":itemDict})
     
 def menuEdit(request):
+    if request.method == 'POST':
+        if 'delItem' in request.POST:
+	  print request.POST
+	  MenuItem.objects.filter(name=request.POST['delItem']).delete()
+	  return HttpResponseRedirect(".")
+	elif 'addItem' in request.POST:
+	  try:
+	      priceVal = int(request.POST['price'])
+	      pPriceVal = int(request.POST['pPrice'])
+	      catId = int(request.POST['cat'])
+	  except ValueError:
+	      return HttpResponseRedirect(".")
+	  cat = Category.objects.filter(id=catId)[0]
+	  try:
+	    item = MenuItem(name=request.POST['name'], price=priceVal, pPrice=pPriceVal, category=cat)
+	    item.save()
+	  except IntegrityError:
+	    return HttpResponseRedirect(".")
+	elif 'addCat' in request.POST:
+	  try:
+	    cat = Category(name=request.POST['cat'])
+	    cat.save()
+	  except IntegrityError:
+	    return HttpResponseRedirect(".")
+	elif 'delCat' in request.POST:
+	  cat = Category.objects.filter(name=request.POST['delCat'])
+	  print cat
+	  print MenuItem.objects.filter(category=cat)
+	  MenuItem.objects.filter(category=cat).delete()
+	  cat.delete()
+	  return HttpResponseRedirect(".")
     itemDict = {}
     cats = Category.objects.all()
     for c in cats:
