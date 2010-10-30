f183e350b7309cc7b2c7eef7bb15d254ca00a198
Marko Krause <zeratul2099@googlemail.com>
2010-10-30 20:07:03 +0200

    - cleaned up template with templatetags - added email-icon

--- plist/templatetags/plisttags.py
+++ plist/templatetags/plisttags.py
@@ -11,7 +11,27 @@ def backButton(var):
             </table>"""
             
 
+def drawDepts(var):
+  if var.dept_status == 0:
+    return "<b>"+str(var.depts)+" &euro;</b>"
+  elif var.dept_status == -1 or var.id == 1:
+    return "<b class='green'>"+str(var.depts)+" &euro;</b>"
+  elif var.dept_status == 1:
+    return "<b class='red'>"+str(var.depts)+" &euro;</b>"
+  else:
+    return "<b class='redblink'>"+str(var.depts)+" &euro;</b>"
+    
+def disableButton(var, lock):
+  user = var;
+  if user.id != 1 and (user.dept_status or lock or user.locked) and user.dept_status != -1:
+    return "disabled='disabled'"
+  else:
+    return ""
+   
 backButton.is_safe = True
-
+drawDepts.is_safe = True
+disableButton.is_safe = True
 register = template.Library()
 register.filter('backButton', backButton)
+register.filter('drawDepts', drawDepts)
+register.filter('disableButton', disableButton)
